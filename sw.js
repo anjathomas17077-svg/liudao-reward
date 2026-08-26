2ac72902ee7bb6855c9c6913a7cda038703c2500
zc <570219453@qq.com>
2026-08-26 22:32:01 +0800

    feat: 手机号重复检测

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='liudao-v4';
+const CACHE_NAME='liudao-v5';
 const URLS=['./index.html','./manifest.json'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(URLS)));
